dd60eb17d2ad7118151eeed8b03def3ee4aab0d1
Tim Beckett <timbeckett@ymail.com>
2019-01-27 03:45:38 -0500

    Added pages (about, home, contact) and move templates to root.

--- djangoblog/urls.py
+++ djangoblog/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 from django.contrib import admin
 
 urlpatterns = [
+    url('', include('pages.urls')),
     url(r'^admin/', admin.site.urls),
     path('blog/', include('blog.urls', namespace='blog')),
 ]
